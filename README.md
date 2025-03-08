ec739daccdc52a88719896029ce88f810044fe12
Alex <21338787@studentmail.ul.ie>
2025-03-08 18:47:51 +0000

    actually pull files out

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS4227ProjectService
-https://gitlab.com/c2842/semester-8/cs4227_project/group-work/-/tree/main/prototype/project_service
--- project_service/project_service/utils.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.exceptions import ValidationError
-
-def date_start_before_end(start, end):
-    if start >= end:
-        raise ValidationError({
-            'Invalid date range': 'End date must be after the start date.'
-        })
